c0e0461b0663faac73c18b7c75275d3fa361076c
Alex <53041444+1299902171@users.noreply.github.com>
2020-06-22 18:04:05 +0800

    Add files via upload

---
+++ Login/user.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.15 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `user` (
+	`id` int ,
+	`username` varchar ,
+	`password` varchar ,
+	`email` varchar 
+); 
+insert into `user` (`id`, `username`, `password`, `email`) values('1','lisa','123456','lisa@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('2','mike','234567','mike@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('3','kite','996699','kite@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('5','tonny23','78787823','tonny23@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('6','alex','445512','alex@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('7','lucy','abc123','lucy@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('8','peter','78798','peter@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('9','jerry','ausdi','jerry@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('10','hit','iook66','hit@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('11','pook','3443434','pook@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('12','tee','996341','tee@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('13','schim','888888','hhhh@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('14','schim','888888','oooo@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('15','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('16','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('17','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('18','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('19','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('20','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('21','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('28','kerry','999663','kerry@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('29','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('35','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('36','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('37','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('38','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('39','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('40','schim','888888','schim@163.com');
+insert into `user` (`id`, `username`, `password`, `email`) values('41','schim','888888','schim@163.com');
